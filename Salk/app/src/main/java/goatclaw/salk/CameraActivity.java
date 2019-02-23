7e8523e9d8c00d1b22bb7b7802cb17d6821cdc12
Campos5 <ecampm02@estudiantes.unileon.es>
2019-02-23 10:47:07 +0100

    :goat: Subida aplicación android inicial
    De momento solo tiene una pantalla de carga y muestra la cámara interna

---
+++ Salk/app/src/main/java/goatclaw/salk/CameraActivity.java
@@ -0,0 +1,74 @@
+package goatclaw.salk;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Surface;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class CameraActivity extends Activity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+
+        // Create our Preview view and set it as the content of our activity.
+
+        setCameraDisplayOrientation(this,Camera.CameraInfo.CAMERA_FACING_FRONT );
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c =  Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+            Log.e("CamNull", "getCameraInstance: null", e);
+        }
+        if(c == null)
+            Log.i("CamNull", "null");
+
+        return c; // returns null if camera is unavailable
+    }
+
+    public void setCameraDisplayOrientation(Activity activity, int cameraId) {
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        mCamera.setDisplayOrientation(result);
+    }
+
+}
---
+++ Salk/app/src/main/java/goatclaw/salk/MainActivity.java
@@ -0,0 +1,27 @@
+package goatclaw.salk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnScan = (Button) findViewById(R.id.btnScan);
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent scanActivity = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivity(scanActivity);
+            }
+        });
+
+    }
+}
