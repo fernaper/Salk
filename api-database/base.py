cd5fa6aac90a618088a79a602c492790d65fff04
AntonioBarral <antbg1996@gmail.com>
2019-03-10 17:18:04 +0100

    Echas funciones para obtener palabras  Tanto con una dificultad 'x' como sin ella, de manera aleatoria y teniendo en cuenta el idioma

--- api-database/base.py
+++ api-database/base.py
@@ -3,17 +3,17 @@ import model
 app = Flask(__name__)
 
 
-@app.route('/get_word')
-def get_word():
-    word = model.get_word()
+@app.route('/get_word/<language>')
+def get_word(language):
+    word = model.get_word(language)
     return word
 
-@app.route('/get_word/<difficulty>')
-def get_word_with_difficulty(difficulty):
-    word = model.get_word_with_difficulty(difficulty)
+@app.route('/get_word/<language>/<difficulty>')
+def get_word_with_difficulty(language, difficulty):
+    word = model.get_word_with_difficulty(language, difficulty)
     return word
 
-@app.route('/get_phrase/<difficulty>')
-def get_phrase_with_difficulty(difficulty):
-    phrase = model.get_phrase_with_difficulty(difficulty)
+@app.route('/get_phrase/<language>/<difficulty>')
+def get_phrase_with_difficulty(language, difficulty):
+    phrase = model.get_phrase_with_difficulty(language, difficulty)
     return phrase
