3f9cd01c02f6d8391f9450bd9ac213bc95e3022b
fernaperg <fernaperg@gmail.com>
2019-03-16 14:29:51 +0100

    :sparkles: Mejorado script para generar dataset

--- api-neural-network/utils/generate_images.py
+++ api-neural-network/utils/generate_images.py
@@ -1,7 +1,10 @@
 import sys
 import cv2
+import os
 
-def main(input_path, output_path, char, skip_frames):
+def main(input_path, output_path, char, skip_frames, right_handed=True):
+    if output_path[-1] != '/':
+        output_path += '/'
     cap = cv2.VideoCapture(input_path)
     skip_frames = int(skip_frames)
     count = 0
@@ -11,7 +14,14 @@ def main(input_path, output_path, char, skip_frames):
         if not ret:
             break
         if count % skip_frames == 0:
-            img_name = '{}{}_{}.png'.format(output_path,char,saved)
+            img_name = '{path}{char}/{char}_{num}.png'.format(path=output_path,char=char,num=saved)
+            if not os.path.isdir('{}{}'.format(output_path,char)):
+                os.mkdir('{}{}'.format(output_path,char))
+            while os.path.exists(img_name):
+                saved += 1
+                img_name = '{path}{char}/{char}_{num}.png'.format(path=output_path,char=char,num=saved)
+            if not right_handed:
+                frame = cv2.flip(frame, 0)
             cv2.imwrite(img_name, frame)
             print(img_name)
             saved += 1
@@ -19,7 +29,15 @@ def main(input_path, output_path, char, skip_frames):
     cap.release()
 
 if __name__ == '__main__':
-    if len(sys.argv) > 4:
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    num_params = len(sys.argv)
+    if num_params > 3:
+        right_handed = True
+        skip_frames = 10
+        if num_params > 4:
+            right_handed =  'True' == sys.argv[4]
+        if num_params > 5:
+            skip_frames = sys.argv[5]
+        main(sys.argv[1], sys.argv[2], sys.argv[3], skip_frames, right_handed)
     else:
-        print('Not enough arguments required: input_path, output_path, char, skip_frames')
+        print('Not enough arguments required: input_path, output_path, char')
+        print('Optional arguments: right_handed, skip_frames')
