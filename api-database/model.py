4fa3932b36960bbe0529184960bb92142fea7229
AntonioBarral <antbg1996@gmail.com>
2019-04-04 22:10:50 +0200

    :sparkles: Cambiados metodos llamadas a API para que la app pueda llamar
    Tambien se han añadido metodos para cambiar dificultad e idioma desde la 
    app

--- api-database/model.py
+++ api-database/model.py
@@ -12,25 +12,8 @@ def connection():
     res = requests.get('http://localhost:9200')
     return res
 
-def get_word(language):
-    query = {
-    "query": {
-        "function_score" : {
-          "query" : { "match": {"lang": language}},
-          "random_score" : {}
-        }
-      },
-     "size":1
 
-    }
-
-    res = es.search(index="words", body=query)
-    print(res)
-    return
-
-
-
-def get_word_with_difficulty(difficulty):
+def get_word_with_difficulty(language, difficulty):
     query = {
     "query": {
         "function_score" : {
@@ -58,12 +41,26 @@ def get_word_with_difficulty(difficulty):
      }
 
     res = es.search(index="words",doc_type='_doc', body=query)
-    print(res)
-    return res
+    #print(res)
+    return res["word"]
 
 #TODO
 def get_phrase_with_difficulty(language, difficulty):
     pass
+
+
+
+def get_user(user_name):
+    query = {
+        "query": {
+            "match": {
+                "name": "antonio"
+            }
+        }
+    }
+
+    res = es.search(index="users",doc_type='_doc', body=query)
+    return res["name"]
 
 
 def insert_language(word_dict, lang):
@@ -76,19 +73,35 @@ def insert_language(word_dict, lang):
     return res
 
 
-#TODO (optional)
-def set_difficulty(word):
-    #Assign a difficulty regarding a different parameter (not the length)
-    pass
-
-#TODO
-'''def gendata():
-    mywords = ['foo', 'bar', 'baz']
-    for word in mywords:
-        yield {
-            "_index": "mywords",
-            "_type": "document",
-            "doc": {"word": word},
+def set_language(user_name, lang):
+    query = {
+        "script": {
+            "source": "ctx._source.language='{}'".format(lang),
+            "lang": "painless"
+        },
+        "query": {
+            "match": {
+                "name": user_name
+            }
         }
-    bulk(es, gendata())
-'''
+    }
+
+    res = es.update_by_query(index="users",doc_type='_doc', body=query)
+    return
+
+
+def set_difficulty(user_name, difficulty):
+    query = {
+        "script": {
+            "source": "ctx._source.difficulty='{}'".format(difficulty),
+            "lang": "painless"
+        },
+        "query": {
+            "match": {
+                "name": user_name
+            }
+        }
+    }
+
+    res = es.update_by_query(index="users",doc_type='_doc', body=query)
+    return
