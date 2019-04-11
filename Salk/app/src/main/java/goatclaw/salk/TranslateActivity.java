97fbb7fa9585bc4f1c17092bbdb3a3aad7a793dc
Campos5 <ecampm02@estudiantes.unileon.es>
2019-04-11 18:34:37 +0200

    :sparkles: Hecho gran parte del traductor

--- Salk/app/src/main/java/goatclaw/salk/TranslateActivity.java
+++ Salk/app/src/main/java/goatclaw/salk/TranslateActivity.java
@@ -16,10 +16,12 @@ public class TranslateActivity extends AppCompatActivity {
 
 package goatclaw.salk;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -30,9 +32,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class TranslateActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private Context ctx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +49,7 @@ public class TranslateActivity extends AppCompatActivity
         setContentView(R.layout.activity_navigation_translate);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        ctx =  getApplicationContext();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -50,6 +59,55 @@ public class TranslateActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        final EditText etInput = (EditText) findViewById(R.id.etTranslate);
+        Button btnOK = (Button) findViewById(R.id.btnOK);
+
+        final TableLayout tlTable = (TableLayout) findViewById(R.id.tableLayout);
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String palabra = etInput.getText().toString().split(" ")[0];
+                palabra += " ";
+                Log.i("HEY", palabra);
+                for (int i = 0; i < palabra.length(); i++){
+                    TableRow tr = new TableRow(ctx);
+                    tr.setLayoutParams(new TableRow.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,
+                            TableLayout.LayoutParams.FILL_PARENT));
+
+                    for (int j = 0; j <  palabra.length(); j++) {
+
+                        Log.i("HEY", palabra.substring(0, 1));
+                        ImageView view = new ImageView(ctx);
+                        int id = getResources().getIdentifier("goatclaw.salk:drawable/" + palabra.substring(0, 1), null, null);
+                        view.setImageResource(id);
+                        palabra = palabra.substring(1);
+                        tr.addView(view);
+                    }
+                    tlTable.addView(tr);
+
+                }
+                /*
+                for (int i = 0; i < 5; i++) {
+
+                    TableRow tr = new TableRow(ctx);
+                    tr.setLayoutParams(new TableRow.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,
+                            TableLayout.LayoutParams.WRAP_CONTENT));
+
+                    for (int j = 0; j < 5; j++) {
+
+                        ImageView view = new ImageView(ctx);
+                        int id = getResources().getIdentifier("goatclaw.salk:drawable/" + "a", null, null);
+                        view.setImageResource(id);
+                        tr.addView(view);
+                    }
+                }*/
+            }
+        });
+
     }
 
     @Override
